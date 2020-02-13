8f59e3e7b16f66dbaf3dd79a08bbce2b3759530c
Lucas Bishop <bishoplucas95@gmail.com>
2020-02-13 17:24:42 -0500

    updated pcoa code and figures

--- code/resv_pcoa.R
+++ code/resv_pcoa.R
@@ -3,25 +3,51 @@ library(readxl)
 library(RColorBrewer)
 library(stringr)
 
-metadata <- read_tsv("resv_metadata.txt") %>% 
+metadata1 <- read_tsv("data/mothur/resv_metadata.txt") %>% 
   rename("group" = "Group") %>% 
-  filter(!group %in% c('48hr_DMSOA', '48hr_DMSOB', '48hr_Media',
-                       '48hr_RESA', '48hr_RESB', '48hr_WEA', '48hr_WEB',
-                       'DMSOA', 'DMSOB', 'Media', 'RESA', 'RESB', 'WEA', 'WEB')) %>% 
+  filter(group %in% c('S3', 'S24', 'S26', 'S27', 'S30')) %>% 
   mutate(Hour=factor(Hour))
+
+metadata2 <- read_tsv("data/mothur/resv_metadata.txt") %>% 
+  rename("group" = "Group") %>% 
+  filter(Hour == '48') %>% 
+  filter(Treatment == 'ResveratrolA') %>% 
+  mutate(Hour=factor(Hour))
+
+metadata3 <- read_tsv("data/mothur/resv_metadata.txt") %>% 
+  rename("group" = "Group") %>% 
+  filter(Hour == '48') %>% 
+  filter(Treatment == 'WineExtractA') %>% 
+  mutate(Hour=factor(Hour))
+
+metadata4 <- read_tsv("data/mothur/resv_metadata.txt") %>% 
+  rename("group" = "Group") %>% 
+  filter(Hour == '48') %>% 
+  filter(Treatment == 'ResveratrolB') %>% 
+  mutate(Hour=factor(Hour))
+
+metadata5 <- read_tsv("data/mothur/resv_metadata.txt") %>% 
+  rename("group" = "Group") %>% 
+  filter(Hour == '48') %>% 
+  filter(Treatment == 'WineExtractB') %>% 
+  mutate(Hour=factor(Hour))
+
+relevant_metadata <- rbind(metadata1, metadata2, metadata3, metadata4, metadata5) %>% 
+  select(-R1_filename, -R2_filename) %>% 
+  filter(!group %in% c('48hr_RESA', '48hr_RESB', '48hr_WEA', '48hr_WEB'))
+
 
 pcoa <- read_tsv("final.braycurtis.0.03.lt.pcoa.axes")
 
-metadata_pcoa <- inner_join(metadata, pcoa, by='group')
-
-res_rwe_pcoa <- metadata_pcoa %>%
-  filter(str_detect(Treatment, "Resveratrol"))
+metadata_pcoa <- inner_join(relevant_metadata, pcoa, by='group')
 
 
-ggplot(res_rwe_pcoa, aes(x=axis1, y=axis2, color = Hour, shape = Hour)) +
-  geom_point(size = 4, alpha = 0.5) +
+ggplot(metadata_pcoa, aes(x=axis1, y=axis2, color = Treatment, shape = Hour)) +
+  geom_point(size = 4, alpha = 0.8) +
+  facet_wrap(~ stool_id) +
   coord_fixed() +
-  labs(title="PCoA of Bray-Curtis dissimilarity Between Resv.-treated Samples",
+  labs(title="PCoA of Bray-Curtis dissimilarity",
+       subtitle = "0hr raw vs. RWE and Res",
        x="PCo Axis 1",
        y="PCo Axis 2") +
   theme_classic()
---
+++ code/stacked_48hrcomparison.R
@@ -0,0 +1,116 @@
+library(tidyverse)
+library(broom)
+library(cowplot)
+library(RColorBrewer)
+
+metadata1 <- read_tsv("data/mothur/resv_metadata.txt") %>% 
+  rename("group" = "Group") %>% 
+  filter(group %in% c('S3', 'S24', 'S26', 'S27', 'S30')) %>% 
+  mutate(Hour=factor(Hour))
+
+metadata2 <- read_tsv("data/mothur/resv_metadata.txt") %>% 
+  rename("group" = "Group") %>% 
+  filter(Hour == '48') %>% 
+  filter(Treatment == 'ResveratrolA') %>% 
+  mutate(Hour=factor(Hour))
+
+metadata3 <- read_tsv("data/mothur/resv_metadata.txt") %>% 
+  rename("group" = "Group") %>% 
+  filter(Hour == '48') %>% 
+  filter(Treatment == 'WineExtractA') %>% 
+  mutate(Hour=factor(Hour))
+
+metadata4 <- read_tsv("data/mothur/resv_metadata.txt") %>% 
+  rename("group" = "Group") %>% 
+  filter(Hour == '48') %>% 
+  filter(Treatment == 'ResveratrolB') %>% 
+  mutate(Hour=factor(Hour))
+
+metadata5 <- read_tsv("data/mothur/resv_metadata.txt") %>% 
+  rename("group" = "Group") %>% 
+  filter(Hour == '48') %>% 
+  filter(Treatment == 'WineExtractB') %>% 
+  mutate(Hour=factor(Hour))
+
+relevant_metadata <- rbind(metadata1, metadata2, metadata3, metadata4, metadata5) %>% 
+  select(-R1_filename, -R2_filename) %>% 
+  filter(!group %in% c('48hr_RESA', '48hr_RESB', '48hr_WEA', '48hr_WEB'))
+                       
+                       
+shared <- read_tsv("data/mothur/final.0.03.subsample.shared") %>% 
+  select(-label, -numOtus) %>% 
+  rename("group" = "Group") %>% 
+  filter(!group %in% c('48hr_DMSOA', '48hr_DMSOB', '48hr_Media',
+                       '48hr_RESA', '48hr_RESB', '48hr_WEA', '48hr_WEB',
+                       'DMSOA', 'DMSOB', 'Media', 'RESA', 'RESB', 'WEA', 'WEB')) %>% 
+  pivot_longer(-group, names_to = 'otu', values_to = 'count') %>% 
+  mutate(relabund = count/1816)
+
+taxonomy <- read_tsv("data/mothur/final.taxonomy") %>% 
+  rename_all(tolower) %>% 
+  # Split taxonomic information into separate columns for each taxonomic level  
+  mutate(taxonomy=str_replace_all(taxonomy, c("\\(\\d*\\)" = "", #drop digits with parentheses around them
+                                              ';$' = "", #removes semi-colon 
+                                              'Bacteria_unclassified' = 'Unclassified',
+                                              "_unclassified" = " Unclassified"))) %>% 
+  # Separate taxonomic levels into separate columns according to semi-colon.
+  separate(taxonomy, into=c("kingdom", "phylum", "class", "order", "family", "genus"), sep=';')
+
+
+shared_taxa <- inner_join(shared, taxonomy)
+
+taxa_metadata <- inner_join(shared_taxa, relevant_metadata)
+
+##Easily readable colors for top 10
+colours = c( "#A54657",  "#582630", "#F7EE7F", "#4DAA57","#F1A66A","#F26157", "#F9ECCC", 
+             "#679289", "#33658A","#F6AE2D","#86BBD8", "#87BBD8")
+
+
+taxa_metadata %>% 
+  ggplot(., aes(x=Treatment, fill=phylum, y=relabund)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~ stool_id) +
+  theme(axis.text.x = element_text(angle = 90, size = 14, colour = "black", vjust = 0.5, hjust = 1, face= "bold"), 
+        axis.title.y = element_text(size = 16, face = "bold"), legend.title = element_text(size = 16, face = "bold"), 
+        legend.text = element_text(size = 12, face = "bold", colour = "black"), 
+        axis.text.y = element_text(colour = "black", size = 12, face = "bold")) + 
+  scale_y_continuous(expand = c(0,0)) + 
+  labs(x = "", y = "Relative Abundance (%)", fill = "Phylum") + 
+  scale_fill_manual(values = colours)
+
+
+##add the broth controls and all the other conditions
+
+
+##plot genus data
+
+genus_data <- taxa_metadata %>% 
+  group_by(group, genus) %>% 
+  summarize(agg_relabund=sum(relabund)) %>% 
+  inner_join(., relevant_metadata, by = "group") %>% 
+  ungroup() 
+
+top_genus <- genus_data %>% 
+  group_by(genus) %>% 
+  summarize(median=median(agg_relabund)) %>% 
+  arrange((desc(median))) %>% 
+  top_n(5, median) %>% 
+  pull(genus)
+
+genus_data %>% 
+  filter(genus %in% top_genus) %>% 
+  mutate(agg_relabund = agg_relabund + 1/2000) %>% 
+  ggplot(aes(x= reorder(genus, -agg_relabund), y=agg_relabund, color=Treatment))+
+  geom_hline(yintercept=1/1816, color="gray")+
+  geom_boxplot()+
+  labs(title=NULL, 
+       x=NULL,
+       y="Relative abundance (%)")+
+  scale_y_log10(breaks=c(1e-4, 1e-3, 1e-2, 1e-1, 1), labels=c(1e-2, 1e-1, 1, 10, 100))+
+  theme(axis.text.x = element_text(angle=45, hjust=1))
+
+
+
+
+
+
