45b84571e8e6e671fb958a18061d2fc37826e50b
Lucas Bishop <bishoplucas95@gmail.com>
2020-02-12 20:08:53 -0500

    edit code so relabund all add to 100

--- code/stool_stackbarplot.R
+++ code/stool_stackbarplot.R
@@ -2,8 +2,9 @@ library(tidyverse)
 library(broom)
 library(cowplot)
 library(RColorBrewer)
+library(reshape2)
 
-metadata1 <- read_tsv("data/mothur/resv_metadata.txt") %>% 
+metadata <- read_tsv("data/mothur/resv_metadata.txt") %>% 
   rename("group" = "Group") %>% 
   filter(group %in% c('S3', 'S24', 'S26', 'S27', 'S30')) %>% 
   mutate(Hour=factor(Hour))
@@ -26,22 +27,23 @@ taxonomy <- read_tsv("data/mothur/final.taxonomy") %>%
   separate(taxonomy, into=c("kingdom", "phylum", "class", "order", "family", "genus"), sep=';')
 
 ##Easily readable colors for top 10
-colours = c( "#A54657",  "#582630", "#F7EE7F", "#4DAA57","#F1A66A","#F26157", "#F9ECCC", "#679289", "#33658A",
-                         "#F6AE2D","#86BBD8")
+colours = c( "#A54657",  "#582630", "#F7EE7F", "#4DAA57","#F1A66A","#F26157", "#F9ECCC", 
+             "#679289", "#33658A","#F6AE2D","#86BBD8", "#87BBD8")
 
 
-shared_taxa <- inner_join(shared, taxonomy) %>%
-  group_by(group, otu) %>% 
-  summarize(agg_relabund=sum(relabund)) %>% 
-  ungroup()
+shared_taxa <- inner_join(shared, taxonomy)
 
-top_otus <- shared_taxa %>% 
-  group_by(otu) %>% 
-  summarize(median=median(agg_relabund)) %>% 
-  arrange((desc(median))) %>% 
-  top_n(n=10, median) %>% 
-  pull(otu)
 
+shared_taxa %>% 
+  ggplot(., aes(x=group, fill=phylum, y=relabund)) +
+  geom_bar(stat = "identity") +
+  theme(axis.text.x = element_text(angle = 90, size = 14, colour = "black", vjust = 0.5, hjust = 1, face= "bold"), 
+        axis.title.y = element_text(size = 16, face = "bold"), legend.title = element_text(size = 16, face = "bold"), 
+        legend.text = element_text(size = 12, face = "bold", colour = "black"), 
+        axis.text.y = element_text(colour = "black", size = 12, face = "bold")) + 
+  scale_y_continuous(expand = c(0,0)) + 
+  labs(x = "", y = "Relative Abundance (%)", fill = "Phylum") + 
+  scale_fill_manual(values = colours)
 
 
 
